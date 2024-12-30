a46a5bbe9c4b5175acbf54d7c452f5fbfe2e2d21
Andreas Nilsson <nilsiker94@gmail.com>
2024-12-30 12:30:36 +0100

    chore: reflect part 1

--- src/game/Game.cs
+++ src/game/Game.cs
@@ -6,8 +6,7 @@ public partial class Game : Control {
   public Button TestButton { get; private set; } = default!;
   public int ButtonPresses { get; private set; }
 
-  public override void _Ready()
-    => TestButton = GetNode<Button>("%TestButton");
+  public override void _Ready() => TestButton = GetNode<Button>("%TestButton");
 
   public void OnTestButtonPressed() => ButtonPresses++;
 }
